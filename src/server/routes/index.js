a6922bb609d716becdc88ba9b88dc8516b2126b7
Kathy Dieser <katdiese@gmail.com>
2016-03-07 18:23:47 -0700

    added data query to route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var options = {
+
+};
+var pgp = require('pg-promise') (options);
+var connectionString = process.env.DATABASE_URL || 'postgress://localhost:5432/animals';
+var db = pgp(connectionString);
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.any('SELECT * FROM carousel_images')
+  .then(function(data) {
+    console.log(data);
+    res.render('index', {
+      title: 'Express',
+      images: data
+      });
+  })
+
 });
 
 module.exports = router;
