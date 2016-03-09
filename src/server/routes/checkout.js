864cd906cf5bf84301b0c1e49955bce0fcebbd51
Kathy Dieser <katdiese@gmail.com>
2016-03-08 21:41:21 -0700

    added route to get checkout page

--- src/server/routes/checkout.js
+++ src/server/routes/checkout.js
@@ -2,4 +2,10 @@ var express = require('express');
 var router = express.Router();
 var queries = require('../queries/checkout-queries');
 
+router.get('/', function(req, res, next) {
+  res.render('checkout', {
+      title: 'Checkout Page'
+      });
+})
+
 module.exports = router;
