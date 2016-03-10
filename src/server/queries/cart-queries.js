cdf4966e1655fa71627a651c05a4e42abff6cb0b
Kathy Dieser <katdiese@gmail.com>
2016-03-10 11:59:16 -0700

    removed commented out code, deleted unnecessary query file

--- src/server/queries/cart-queries.js
+++ src/server/queries/cart-queries.js
@@ -6,12 +6,11 @@ function Cart() {
 
 module.exports = {
   getCart: function(userID) {
-//     return Cart().join('animals', 'animals.id', '=', 'shopping_carts.animal_id').select('shopping_carts.price', 'shopping_carts.quantity', 'animals.name', 'animals.image').where('user_id', userID);
-  
+
   return knex.raw('SELECT * FROM animals INNER JOIN (SELECT user_id, quantity, animal_id, (quantity * price) as total FROM shopping_carts) as temp ON (animals.id = temp.animal_id) WHERE user_id = 3;');
 
   },
-  
+
   getGrandTotal: function(userID) {
     return knex.raw('SELECT SUM(total) from (SELECT (quantity * price) as total FROM shopping_carts WHERE user_id = ' + userID + ') as t');
   }
