519166591bc367d1baf353fd325cdf824bf2fc07
Kathy Dieser <katdiese@gmail.com>
2016-03-09 22:28:55 -0700

    changed data types of id's to integers to properly reference in queries

--- migrations/20160308201700_create_table_shopping_carts.js
+++ migrations/20160308201700_create_table_shopping_carts.js
@@ -2,11 +2,11 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('shopping_carts', function(table){
     table.increments();
-    table.string('user_id').references('users', 'id');;
-    table.string('animal_id').references('animals', 'id');;
+    table.integer('user_id').references('users', 'id');
+    table.integer('animal_id').references('animals', 'id');
     table.decimal('price');
-    table.integer('quantity')  
-  })  
+    table.integer('quantity') ;
+  })
 };
 
 exports.down = function(knex, Promise) {
